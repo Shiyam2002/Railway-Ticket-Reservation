fabc89057af6fbc1242de20b65f3a5b5a8ddd43a
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2023-09-04 19:39:28 +0530

    Position update

--- Railway_Ticket_Reservation/src/RailwayBookingCounter/TicketBooking.java
+++ Railway_Ticket_Reservation/src/RailwayBookingCounter/TicketBooking.java
@@ -1,6 +1,7 @@
 package RailwayBookingCounter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,6 +20,12 @@ public class TicketBooking {
 	static List<Integer> bookedTicket = new ArrayList<>();
 	static Queue<Integer> racList = new LinkedList<>();
 	static Queue<Integer> watingList = new LinkedList<>();
+	
+	static List<Integer> lowerBerthPosition = new ArrayList<>(Arrays.asList(20));
+	static List<Integer> middleBerthPosition = new ArrayList<>(Arrays.asList(20));
+	static List<Integer> upperBerthPosition = new ArrayList<>(Arrays.asList(20));
+	static List<Integer> racListPosition = new ArrayList<>(Arrays.asList(18));
+	static List<Integer> waitingListPosition = new ArrayList<>(Arrays.asList(10));
 	
 	static Map<Integer, passengerDetails> passenger = new HashMap<>();
 }
